87dc9995f31568f97120d1318c4796b1528e14af
andrey <apin@nm.ru>
2019-11-24 18:02:24 +0300

    Поддержка включения света по датчику движения

--- application.go
+++ application.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/apinchouk/smart-home/dispatcher"
 	"github.com/apinchouk/smart-home/gpio"
+	"github.com/apinchouk/smart-home/sensorled"
 )
 
 type IApplication interface {
@@ -14,6 +15,7 @@ type IApplication interface {
 type Application struct {
 	dispatcher dispatcher.IDispatcher
 	stairWay   StairWayLed
+	sensorLed  sensorled.SensorLed
 }
 
 func (this *Application) init() bool {
@@ -37,6 +39,19 @@ func (this *Application) init() bool {
 	this.stairWay.gpioButtonLed = gpio.GetGpioIn(app.dispatcher, gpio.Pin40, &this.stairWay, button)
 	this.stairWay.gpioControlLed = gpio.GetGpioOut(gpio.Pin3, relay)
 	this.stairWay.Init()
+
+	hallkey := gpio.GpioInParam{
+		Trigger:  gpio.TrigBoth,
+		PullUp:   gpio.PullUpOff,
+		Invert:   false,
+		JitterMs: 100,
+	}
+
+	this.sensorLed = sensorled.SensorLed{Dispatcher: this.dispatcher}
+	this.sensorLed.GpioButtonLed = gpio.GetGpioIn(app.dispatcher, gpio.Pin13, &this.sensorLed, hallkey)
+	this.sensorLed.GpioControlLed = gpio.GetGpioOut(gpio.Pin15, relay)
+	this.sensorLed.Init()
+
 	return true
 }
 
---
+++ sensorled/sensor-led.go
@@ -0,0 +1,125 @@
+package sensorled
+
+import (
+	"fmt"
+	"time"
+	"github.com/apinchouk/smart-home/dispatcher"
+	"github.com/apinchouk/smart-home/gpio"
+//	"github.com/kelvins/sunrisesunset"
+)
+
+type ledState int
+
+const (
+	stateLedDisable         ledState = 0
+	stateLedTemporaryEnable          = 1
+)
+
+type ledEvent int
+
+const (
+	evEntry     ledEvent = 0
+	evButtonOn           = 1
+	evButtonOff          = 2
+	tmProtect            = 3
+	tmEnable             = 4
+	tmDisable            = 5
+)
+
+type SensorLed struct {
+	Dispatcher     dispatcher.IDispatcher
+	GpioButtonLed  gpio.IGpioIn
+	GpioControlLed gpio.IGpioOut
+	state          ledState
+	timer          dispatcher.ITimer
+}
+func isNight() bool {
+	t:=time.Now()
+	sunset,_:=time.Parse("15:04:05", "17:00:00")
+	sunrise,_:=time.Parse("15:04:05", "06:00:00")
+	//fmt.Println("Time:", t.Format("15:04:05")) // Sunset: 18:14:27
+/*
+	p := sunrisesunset.Parameters{
+		Latitude:  53.195682,
+		Longitude: 45.010420,
+		UtcOffset: 3.0,
+		Date:      t,
+	  }
+	  sunrise, sunset, err := p.GetSunriseSunset()
+
+	  if err == nil {
+		fmt.Println("Sunrise:", sunrise.Format("15:04:05")) // Sunrise: 06:11:44
+		fmt.Println("Sunset:", sunset.Format("15:04:05")) // Sunset: 18:14:27
+	} else {
+		fmt.Println(err)
+	}
+	*/
+	fmt.Printf("Time:%v %v %v\n", t.Hour(),sunrise.Hour(),sunset.Hour())
+	if ((t.Hour()>=sunset.Hour() && t.Minute()>=sunset.Minute()) || 
+	(t.Hour()<=sunrise.Hour() && t.Minute()<=sunrise.Minute())) {
+		fmt.Println("Night\n")
+		return true
+	} else {
+		fmt.Println("Day\n")
+	}
+	return false
+}
+func (this *SensorLed) Init() {
+	
+	this.timer = this.Dispatcher.CreateTimer(this)
+	this.onLedDisable(evEntry, nil)
+}
+func (this *SensorLed) OnGpioEvent(pin gpio.EnPINS2BCM, value int) {
+
+	if value == 0 {
+		this.onEvent(evButtonOff, nil)
+	} else {
+		this.onEvent(evButtonOn, nil)
+	}
+
+}
+
+// not implemented
+func (this *SensorLed) OnDispatcherEvent(data dispatcher.IEventData) {
+
+}
+func (this *SensorLed) OnDispatcherTimer(id int, data dispatcher.IEventData) {
+	this.onEvent(ledEvent(data.(int)), nil)
+}
+func (this *SensorLed) onEvent(event ledEvent, data dispatcher.IEventData) {
+	fmt.Printf("SensorLed: %v, State=%d, onEvent=%d\n", time.Now(), this.state, event)
+
+	switch this.state {
+	case stateLedDisable:
+		this.onLedDisable(event, data)
+	case stateLedTemporaryEnable:
+		this.onLedTemporatyEnable(event, data)
+	
+	}
+
+}
+func (this *SensorLed) onLedDisable(event ledEvent, data dispatcher.IEventData) {
+	switch event {
+	case evEntry:
+		fmt.Println("Disable")
+		this.state = stateLedDisable
+		this.GpioControlLed.SetValue(0)
+	case evButtonOn:
+		if (isNight()) {
+			this.onLedTemporatyEnable(evEntry, nil)
+		}
+	}
+}
+func (this *SensorLed) onLedTemporatyEnable(event ledEvent, data dispatcher.IEventData) {
+	switch event {
+	case evEntry:
+		fmt.Println("TemporaryEnable")
+		this.state = stateLedTemporaryEnable
+		this.GpioControlLed.SetValue(1)
+//		this.timer.Start(time.Second*30, tmDisable, dispatcher.TimerSingle)
+	case evButtonOff:
+		this.onLedDisable(evEntry, nil)
+//	case tmDisable:
+//		this.onLedDisable(evEntry, nil)
+	}
+}
